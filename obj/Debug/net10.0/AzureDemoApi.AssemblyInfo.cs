0c26df0cc3c26826f81c2c00bd01e8466de622fa
Fatima Davoudi <fa.davodi@gmail.com>
2026-05-13 10:35:16 +0200

    add github actions deploy

--- obj/Debug/net10.0/AzureDemoApi.AssemblyInfo.cs
+++ obj/Debug/net10.0/AzureDemoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AzureDemoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57eb8459c9aad46c995e09f03cbbdbddfd7cc61b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10c377fd043cbfe54518ec225e8934aab8465158")]
 [assembly: System.Reflection.AssemblyProductAttribute("AzureDemoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AzureDemoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
